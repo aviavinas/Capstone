fc179e984f4aef4aa067a8131cd742a5b0dad679
Avinash Kumar <aviavinash.official@gmail.com>
2019-12-01 19:11:34 +0530

    weather col working

--- nw/predictions api.py
+++ nw/predictions api.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -6,27 +7,23 @@ from keras.models import load_model
 from keras.utils.data_utils import get_file
 from google.cloud import firestore
 
-def engine(request):
+def engineApi(request):
     args = request.args.to_dict()
     print(args)
     db = firestore.Client()
     item = db.collection('product').document(request.args.get("id")).get().to_dict()
 
-    def data(time, Weather, day):
-        daylist=[0,0,0,0,0,0,0]
+    def data(time, Weather):
+        days=[0,0,0,0,0,0,0]
+        day = datetime.datetime.today().weekday()
+        days[day]=1
+        return np.array([[time,days[0],days[1],days[2],days[3],days[4],days[5],days[6],Weather]])
 
-        def check(daylist,day):
-            daylist[day-1]=1
-            return daylist
-
-        newdaylist=check(daylist,day)
-        return np.array([[time,newdaylist[0],newdaylist[1],newdaylist[2],newdaylist[3],newdaylist[4],newdaylist[5],newdaylist[6],Weather]])
-
-    def prediction(time, Weather, day):
+    def prediction(time, Weather):
         loaded_model = tf.keras.models.load_model(get_file('modelFile', item['model']))
-        predictions=np.round(loaded_model.predict(data(time, Weather, day)))
+        predictions=np.round(loaded_model.predict(data(time, Weather)))
         result = str(int(predictions[0][0]))
         print("Result @ ",time,":",result)
         return result
 
-    return prediction(int(args['time']), int(args['weather']), int(args['day']))
+    return prediction(int(args['time']), int(args['weather']))
--- www/public/js/main.js
+++ www/public/js/main.js
@@ -86,11 +86,42 @@ function analyze() {
     });
 }
 
+function getProdList() {
+    var htm = "";
+    db.collection("product").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            htm+= "<tr data-id='"+doc.id+"'><td>"+doc.data().name+"</td><td>"+doc.data().sold+"</td><td>"+doc.data().inventory+"</td></tr>"
+        });
+        $('#prod').html(htm);
+        $("#prod tr").click(function(){handleSelect(this)});      
+        showWeather();
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+}
+
+function handleSelect(k) {
+    const id = $(k).attr("data-id");
+    $.get("https://salest.firebaseapp.com/engine", (r) => console.log(r))
+}
+
+function showWeather() {
+    var curHour = new Date().getHours();
+    curHour = curHour<10?10:curHour;
+    curHour = curHour>22?22:curHour;
+    var remH = 22-curHour;
+
+    $.get("https://api.weatherbit.io/v2.0/forecast/hourly?city=Phagwara,IN&key=3a1f4eb49895428eadf479902b542b54&hours="+remH, (r) => {
+        var htm="";
+        for(i=curHour; i<22; i++) {
+            isGoodWeather = r.data[i-curHour].weather.code>=700;
+            htm+= '<div class="col-1"><img src="'+(isGoodWeather?"sun":"cloud")+'.png"/>'+(i==12?12:i%12)+' '+(i>=12?"PM":"AM")+'</div>';
+        }
+        $("#weatherMap").html(htm);            
+    })
+}
+
 analyze();
-getPred("DQRP4NDTzWkRGZmnK8Ol");
 drawChart([0,5,63,41,95,28,55,89,34]);
-
-
-
-// Call functions
-$.get("https://salest.firebaseapp.com/engine", (r) => console.log(r))
+getProdList();
